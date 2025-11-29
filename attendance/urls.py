8ec8916aa7a81e45d849f8380615b1cc00ac67b9
Erish <irishmjn@gmail.com>
2025-11-29 20:59:36 +0545

    Updated the urls by correcting views

--- attendance/urls.py
+++ attendance/urls.py
@@ -1,7 +1,13 @@
-from .views import LoginAPI, UserRegistrationView, VerifyEmail
+from .views import LoginAPI, UserRegistrationView, VerifyEmail, AttendanceViewset
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'attendance', AttendanceViewset, basename='attendance')
 urlpatterns = [
     path('login/', LoginAPI.as_view(), name='login'),
     path('register/', UserRegistrationView.as_view(), name='signup'),
     path("verify/<path:token>/",VerifyEmail.as_view())
 ]
+
+urlpatterns += router.urls
