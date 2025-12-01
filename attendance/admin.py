26e7f557e7c7f4b6c60f96a0639827b52a79472c
Erish <irishmjn@gmail.com>
2025-12-01 14:25:57 +0545

    removed the property for user and save function

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
+from django import forms
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import *
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Department)
-admin.site.register(Employee)
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    pass
 admin.site.register(AttendanceRecord)
 admin.site.register(LeaveRequest)
