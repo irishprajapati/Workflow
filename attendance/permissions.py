453eba177c1407e702882fba148fe7c886ddca9b
Erish <erish968@gmail.com>
2026-01-14 18:34:06 +0545

    Refactor: Code of models in employee added as property

--- attendance/permissions.py
+++ attendance/permissions.py
@@ -1,20 +1,26 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework.permissions import BasePermission
 from .models import *
-from rest_framework import status
-from django.http import response
 def get_employee(user):
     if not user or not user.is_authenticated:
         return None
     return getattr(user, "Employee_profile", None)
 
+class HasEmployeeProfile(BasePermission):
+    def has_permission(self, request, view):
+        return get_employee(request.user) is not None
 class IsEmployee(BasePermission):
     def has_permission(self, request, view):
         emp = get_employee(request.user)
-        return emp is not None and emp.role == "EMPLOYEE"
+        return emp and emp.role == "EMPLOYEE"
+
 class IsOfficial(BasePermission):
+    """
+    HR / Admin / Manager
+    Has authority to access sensitive employee data
+    """
     def has_permission(self, request, view):
         emp = get_employee(request.user)
-        return emp is not None and emp.role in ["HR", "ADMIN", "MANAGER"]
+        return emp and emp.is_official
 
 class IsEmployeeOrIsOfficial(BasePermission):
     def has_permission(self, request, view):
@@ -22,3 +28,11 @@ class IsEmployeeOrIsOfficial(BasePermission):
         if not emp:
             return False
         return emp.role in ['EMPLOYEE', 'HR', 'ADMIN', 'MANAGER']
+
+class IsOwnerOrOfficial(BasePermission):
+    def has_permission(self, request, view):
+        return get_employee(request.user) is not None
+
+    def has_object_permission(self, request, view, obj):
+        emp = get_employee(request.user)
+        return emp.is_official or obj.employee == emp
